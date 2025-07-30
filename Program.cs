20e276139aa9e912e9bf022cd5f393e9ef0d6d5f
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-07-30 04:48:12 -0400

    Apply EF Core Migrations in Docker

--- Program.cs
+++ Program.cs
@@ -11,10 +11,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
 var app = builder.Build();
+
+Thread.Sleep(20000);
+
+// Auto-migrate
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
